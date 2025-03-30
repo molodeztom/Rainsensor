560f9ff49b624182cbe0aa907a94ca5c7a5a8000
Tom_Freier <Thomas@freier-online.de>
2025-03-30 18:55:35 +0200

    Code cleaning

--- main/rainsensor_main.c
+++ main/rainsensor_main.c
@@ -33,6 +33,7 @@ RainSensor
   20250327  V0.6.5          Debug out edge_count
   20250330  V0.6.5.1        Now working fine, still need to wake on timer only when a pulse was detected
   20250330  V0.6.5.2        Only wake on timer when at least one pulse detected
+  20250330  V0.6.5.3        Clean code
 
   */
 
@@ -60,7 +61,7 @@ static const char *TAG = "rainsens";
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 #define RTC_SLOW_CLK_FREQ 136000 // when RTC_CLCK Source = internal 136 kHz oscillator
 // #define RTC_SLOW_CLK_FREQ 68359 //when RTC_CLCK Source = internal 17.5 MHz oscillator / 256
-#define ulp_wakeup_period 2000                    // after ulp is halted it sleeps until next wakeup period
+#define ulp_wakeup_period 1000                    // after ulp is halted it sleeps until next wakeup period
 static const double wakeup_interval_seconds = 30; // time to wake cpu if at minimum one input pulse detected
 // external references
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
@@ -105,7 +106,7 @@ void app_main(void)
      */
     vTaskDelay(pdMS_TO_TICKS(1000));
     esp_log_level_set("*", ESP_LOG_INFO);
-    printf("rainsensor V0.6.5.2 \n\n");
+    printf("rainsensor V0.6.5.3 \n\n");
     printf("Firmware Version: %s\n", APP_VERSION);
 
     /* Configure the peripheral according to the LED type */
@@ -157,9 +158,8 @@ static void init_ulp_program(void)
      *
      * Note that the ULP reads only the lower 16 bits of these variables.
      */
-    ulp_debounce_counter = 3;
+   // ulp_debounce_counter = 3;
     ulp_debounce_max_count = 3;
-    ulp_next_edge = 0;
     ulp_io_number = rtcio_num; /* map from GPIO# to RTC_IO# */
     ulp_edge_count_to_wake_up = 8;
 
